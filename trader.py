07bd65ffcb110f41090afc20a1f99dd79212173f
ga639487 <whos2637@gmail.com>
2018-03-13 20:21:19 +0800

    last

--- trader.py
+++ trader.py
@@ -22,12 +22,13 @@ if __name__ == '__main__':
     
     import csv
     
-    f = open('testing_data.csv', 'r')
     buy = 0
     status = 0
     day = 0
-    output = []
     data = []
+
+    f = open(args.testing, 'r')  
+  
     for row in csv.reader(f):
         data.append(row[0])
         data.append(row[1])
@@ -35,20 +36,19 @@ if __name__ == '__main__':
         data.append(row[3])
         day+=1
     f.close()
-    output.append(0)
-    for i in range(1, day-1):
-        if status != 1 and data[i*4-3]+data[i*4-2] > data[i*4-1]*2:
-            status+=1
-            buy = data[i*4]
-            output.append(1)
-        elif status != -1 and data[i*4] > buy:
-            status-=1
-            output.append(-1)
-        else:
-            output.append(0)
-
-    print (output)
-    print (len(output))
-    with open('output.csv', 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(output)
+    with open('output.csv', 'w') as csv_writer:
+        csv_writer.write('0')
+        csv_writer.write('\n')
+        for i in range(1, day-1):
+            if status != 1 and data[i*4-3]+data[i*4-2] > data[i*4-1]*2:
+                status+=1
+                buy = data[i*4]
+                csv_writer.write('1')
+                csv_writer.write('\n')
+            elif status != -1 and data[i*4] > buy:
+                status-=1
+                csv_writer.write('-1')
+                csv_writer.write('\n')
+            else:
+                csv_writer.write('0')
+                csv_writer.write('\n')
